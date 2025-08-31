a6f00d36875cfb39f367fff8b0205eb129431f39
iamharshtambe <harshtambe.dev@gmail.com>
2025-08-31 22:20:43 +0530

    add and update user

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello, World!');
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function createUser() {
+  await prisma.user.update({
+    where: { id: 1 },
+    data: { username: 'Sumit', password: 'Sumit@1455', age: 22 },
+  });
+}
+
+createUser();
