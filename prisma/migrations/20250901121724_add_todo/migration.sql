d0d0b930481aea0a97b986903bf73c51e801cd9a
iamharshtambe <harshtambe.dev@gmail.com>
2025-09-01 17:52:42 +0530

    add todo and relations

---
+++ prisma/migrations/20250901121724_add_todo/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "public"."Todo" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "status" BOOLEAN NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "public"."Todo" ADD CONSTRAINT "Todo_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
