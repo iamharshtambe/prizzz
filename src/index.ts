018b31ce7bf1997c6873449db78c01ebf5300665
iamharshtambe <harshtambe.dev@gmail.com>
2025-09-01 18:15:54 +0530

    find user

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-async function createUser() {
-  await prisma.user.update({
+async function getUser() {
+  const user = await prisma.user.findFirst({
     where: { id: 1 },
-    data: { username: 'Sumit', password: 'Sumit@1455', age: 22 },
+    include: { todos: true },
   });
+
+  console.log(user);
 }
 
-createUser();
+getUser();
